c480638bddbdd3248281c3e7e04980c176bd6080
NIKOLETA DOUMITSA <ndoumitsa@ece.auth.gr>
2026-03-05 14:26:11 +0200

    Clean up SQL script by removing blank lines
    Removed unnecessary blank lines from the SQL script.

--- ddl_and_dml_in_sql/table_creation/query.sql
+++ ddl_and_dml_in_sql/table_creation/query.sql
@@ -1,18 +1,9 @@
--- Write your solution here
 CREATE TABLE library (
     id INT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     author VARCHAR(50),
     pages INT
 );
-
-
-
-
-
--- Please do not modify the code below!
--- It is necessary for the correct execution of the task. 
--- Later, we will review everything written below; you don't need it right now.
 
 INSERT INTO library (id, title, author, pages) VALUES 
   (1, 'CAMINO GHOSTS', 'John Grisham', '213'),
